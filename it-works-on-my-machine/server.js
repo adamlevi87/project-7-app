c932db1bfe6c334762d06bd5f04fa0e9c8fe4ec2
Adam Levi <adamlevi87@gmail.com>
2025-09-22 11:24:41 +0300

    Update 1411107870

--- it-works-on-my-machine/server.js
+++ it-works-on-my-machine/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const csrf = require("@dr.pogodin/csurf");
+const csrf = csrf;
 const app = express();
 
 // Session middleware with secure settings
